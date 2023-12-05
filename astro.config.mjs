2ed9b5db223e359fd96f5b3ccb2bdc19b17d70f4
olef <tipa-knight@yandex.ru>
2023-12-05 17:43:03 +0500

    robot.txt, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://email-verstka.netlify.app',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), robotsTxt(), sitemap()],
   image: {
     service: {
       entrypoint: 'astro/assets/services/noop'
